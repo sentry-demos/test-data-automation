300fae9f6d3ee98c392340580e503259283b2fec
William Capozzoli <thinkocapo@gmail.com>
2021-04-26 19:13:29 -0700

    check that buttons are loaded, handle exception if not

--- frontend_tests/test_add_to_cart.py
+++ frontend_tests/test_add_to_cart.py
@@ -2,22 +2,29 @@ import pytest
 import time
 import yaml
 import random
-from sentry_sdk import set_tag
+import sentry_sdk
 
 @pytest.mark.usefixtures("driver")
 def test_add_to_cart(driver):
-    set_tag("test", "test_add_to_cart")
+    sentry_sdk.set_tag("test", "test_add_to_cart")
     with open('endpoints.yaml', 'r') as stream:
         data_loaded = yaml.safe_load(stream)
         endpoints = data_loaded['react_endpoints']
 
     for endpoint in endpoints:
+        reported = False
         for i in range(random.randrange(20)):
             driver.get(endpoint)
 
-            buy_button = driver.find_element_by_css_selector('.item button')
-            for i in range(random.randrange(3) + 3):
-                buy_button.click()
+            # Buttons not be available if tools did not load in time
+            try:
+                buy_button = driver.find_element_by_css_selector('.item button')
+                for i in range(random.randrange(3) + 3):
+                    buy_button.click()
+                driver.find_element_by_css_selector('.sidebar button').click()
+            except Exception as err:
+                if reported == False:
+                    sentry_sdk.capture_exception(err)
+                reported = True
 
-            driver.find_element_by_css_selector('.sidebar button').click()
             time.sleep(random.randrange(3) + 3)
